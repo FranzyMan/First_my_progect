2ecc630d58633439b35a95801a9e714e7ed786b3
MacBook <macbook@MacBook-Pro-User.local>
2022-06-18 15:25:21 +0500

    Test_array

---
+++ src/main/java/com/github/franzyman/first_my_project/UiApi.java
@@ -0,0 +1,8 @@
+package com.github.franzyman.first_my_project;
+
+public class UiApi {
+    List<String> list = new ArrayList<>();
+    list.add('ui');
+    list.add('api');
+    System.out.println(list);// На экране увидим:[ui6, api]
+}
